1181cd04bc2c2fa72ae80039dc1155b89225f9ad
programmer <programmer@BB21004>
2018-04-23 17:01:50 -0500

    2nd commit

---
+++ TestProjectUI/TestProjectUI/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestProjectUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // The following is the show "HELLO WORLD" to the End-User
+            Console.WriteLine("Hello World");
+
+        }
+    }
+}
